6208c41f200917eb1f918cb6b234eac47b0cb491
Rim-Tfayli <Rim.tf.2004@hotmail.com>
2025-11-04 16:18:29 +0200

    axios.js  added

--- Backend/get.php
+++ Backend/get.php
@@ -1,6 +1,21 @@
 <?php
     $con = mysqli_connect('localhost', 'root', '', 'solitaire');
+
     if(!$con)
         die('<p> general error occurred</p>');
-    $query = 'select * from players order by score DESC ';
+    $query = "select * from players ".
+              "ORDER BY score DESC";
+
+    $result = mysqli_query($con, $query);
+
+    if(!$result)
+        die("No recorders");
+
+    $players = [];
+
+    while ($row = mysqli_fetch_assoc($result)) {
+        $players[] = $row;
+    }
+
+    echo json_encode($players);
 ?>
---
+++ Frontend/script.js
@@ -0,0 +1,33 @@
+function getTopPlayers(){
+    axios.get('http://localhost/SEFactory/Project1/Backend/get.php')
+    .then(res => {
+        const players = res.data;
+        const table = document.getElementById("topPlayers");
+        const medal = '<i class="fas fa-medal"></i>';
+        players.forEach( (player,i) => {
+                while(i<10){
+                    const row = document.createElement("tr");
+                    if(i<3){
+                        row.innerHTML = `
+                            <td>${medal} ${i+1}</td>
+                            <td>${player.Name}</td>
+                            <td>${player.score}</td>
+                        `;
+                    }
+                    else{
+                        row.innerHTML = `
+                            <td>${$i+1}</td>
+                            <td>${player.Name}</td>
+                            <td>${player.score}</td>
+                        `;
+                    }
+                    table.appendChild(row);
+                }              
+        });
+    })
+    .catch(error => {
+      console.error("Error while getting data:", error);
+    });
+}
+
+window.onload = getTopPlayers;
