4b34fefc0d4ca4e6f21a1fd03fc27e52e3ebc4bf
Rim-Tfayli <Rim.tf.2004@hotmail.com>
2025-11-05 12:12:27 +0200

    frontend fixes, add new player page

--- Frontend/script.js
+++ Frontend/script.js
@@ -1,10 +1,11 @@
+const medal = '<i class="fas fa-medal"></i>';
+
 function getTopPlayers(){
     axios.get('http://localhost/SEFactory/Project1/Backend/get.php')
     .then(res => {
         const players = res.data;
         console.log(players);
         const table = document.getElementById("topPlayers");
-        const medal = '<i class="fas fa-medal"></i>';
         players.forEach( (player,i) => {
                 while(i<10){
                     const row = document.createElement("tr");
@@ -30,5 +31,36 @@ function getTopPlayers(){
       console.error("Error while getting data:", error);
     });
 }
+
+function getAllPlayers(){
+    axios.get('http://localhost/SEFactory/Project1/Backend/get.php')
+    .then(res => {
+        const players = res.data;
+        const table = document.getElementById("Players");
+        players.forEach( (player,i) => {            
+                    const row = document.createElement("tr");
+                    if(i<3){
+                        row.innerHTML = `
+                            <td>${medal} ${i+1}</td>
+                            <td>${player.Name}</td>
+                            <td>${player.score}</td>
+                        `;
+                    }
+                    else{
+                        row.innerHTML = `
+                            <td>${$i+1}</td>
+                            <td>${player.Name}</td>
+                            <td>${player.score}</td>
+                        `;
+                    }
+                    table.appendChild(row);
+        });
+    })
+    .catch(error => {
+      console.error("Error while getting data:", error);
+    });
+}
+
+
 
 window.onload = getTopPlayers;
