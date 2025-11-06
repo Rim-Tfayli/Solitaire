6211c3bd9f99d5ba1bcaa98f122d5b94ec23f6bf
Rim-Tfayli <Rim.tf.2004@hotmail.com>
2025-11-06 16:02:42 +0200

    fixing merge conflicts

--- Backend/connection/connection.php
+++ Backend/connection/connection.php
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 <?php 
 
 $db_host = "localhost";
@@ -7,14 +6,4 @@ $db_pass = null;
 $db_name = "solitaire"; 
 
 $mysql = new mysqli($db_host, $db_user, $db_pass, $db_name);
-
-=======
-<?php
-$db_host = "localhost";
-$db_user = "root";
-$db_pass = null;
-$db_name = "solitaire";
-
-$mysql = new mysqli($db_host, $db_user, $db_pass, $db_name);
->>>>>>> cd-scores-page
 ?>
